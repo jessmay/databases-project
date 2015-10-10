3a3488787f962382f41c7765e935d0841281508a
Cindy-H <charn@knights.ucf.edu>
2015-10-09 22:04:17 -0400

    Changed time to use spinner

--- event/create/index.php
+++ event/create/index.php
@@ -4,10 +4,53 @@
 	<script>
 		$(function() {
 			$( "#datepicker" ).datepicker();
+            $.widget("ui.hourspin", $.ui.spinner, {
+                _parse: function(value) {
+                    if (typeof value === "string") {
+                        var pm = value.split(' ')[1] == "PM";
+                        var hour = parseInt(value);
+                        if (hour == 12) {
+                            return pm * 12;
+                        }
+                        return hour + (pm ? 12 : 0);
+                    } else {
+                        return value;
+                    }
+                },
+                _format: function(value) {
+                    if (value < 12) {
+                        if (value == 0) {
+                            return "12 AM";
+                        }
+                        return value + " AM";
+                    } else {
+                        if (value == 12) {
+                            return "12 PM";
+                        }
+                        return (value - 12) + " PM";
+                    }
+                }
+            });
+            $( "#timespinner" ).hourspin({
+                spin: function(event, ui) {
+                    if (ui.value > 23) {
+                        $(this).hourspin("value", 0);
+                        return false;
+                    }
+                    else if (ui.value < 0) {
+                        $(this).hourspin("value", 23);
+                        return false;
+                    }
+                }
+            });
 		});
 	</script>
 
 <?php include TEMPLATE_MIDDLE; ?>
+    <h2>
+        Create Event
+    </h2>
+    <hr>
 	<p>
 		<form>
 			<div class="row">
@@ -50,8 +93,7 @@
 				<div class="col-md-2"></div>
 				<div class="col-md-4 form-group">
 					<label for="eventTime">Time</label>
-					<!-- TODO: Change to spinner of hours -->
-					<input type="text" class="form-control" id="eventTime" placeholder="ex: 03:00 PM">
+					<input type="text" id="timespinner" name="timespinner" value="12 PM">
 				</div>
 			</div>
 			
@@ -68,12 +110,12 @@
 				<div class="col-md-2">
 					<input type="radio" name="eventTypes" id="eventTypes3" value="type3"> RSO
 				</div>
-			</div><br />
+			</div><br>
 						
 			<div class="row">
 				<div class="col-md-4 form-group">
 					<label for="eventContactName">Contact Name</label>
-					<input type="email" class="form-control" id="eventContactName" placeholder="Name">
+					<input type="text" class="form-control" id="eventContactName" placeholder="Name">
 				</div>
 				<div class="col-md-4 form-group">
 					<label for="eventContactEmail">Contact Email</label>
@@ -81,9 +123,9 @@
 				</div>
 				<div class="col-md-4 form-group">
 					<label for="eventContactPhone">Contact Phone</label>
-					<input type="email" class="form-control" id="eventContactPhone" placeholder="ex: (123) 456-7890">
+					<input type="tel" class="form-control" id="eventContactPhone" placeholder="ex: (123) 456-7890">
 				</div>
-			</div><br />
+			</div><br>
 			
 			<button type="submit" class="btn btn-primary">Submit</button>
 		</form>
