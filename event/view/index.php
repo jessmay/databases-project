fefb3f09035f7769e30b87a3fcc176fe24fcbd6b
jessmay <jessica.may.cs@knights.ucf.edu>
2015-10-12 14:34:32 -0400

    View event layout example.

--- event/view/index.php
+++ event/view/index.php
@@ -1,10 +1,27 @@
 <?php include '../../init.php'; ?>
 <?php include TEMPLATE_TOP; ?>
-	<title>Front Page Example</title>
+	<title>Event View Page</title>
 
 <?php include TEMPLATE_MIDDLE; ?>
+	<h2>EVENT NAME HERE</h2>
+    <h4>DateTime, Location</h4>
+    <hr>
+	<h4>Category</h4>
+	<p>Awesome</p>
+	<br>
+	<h4>Event Description</h4>
 	<p>
-		Hey, content!
+		Lorem ipsum dolor sit amet, vero probo illum qui ad, ad mutat congue mei. Cum ei natum malis deserunt. Ei mea vide alienum partiendo. Sea ex justo numquam, vide fastidii insolens usu ad, erat molestiae constituam quo te.
+		Eu sit vidisse aperiam. Et per dicta harum melius, eos bonorum vocibus denique et. Te facer sonet postulant usu. No eos habeo labitur. Cum justo soluta te, sed ut error oportere mediocrem.
+		Id cum illud congue erroribus, audire dissentiet vel id. Mucius latine mandamus qui no, est erat noster accommodare at. Sale iracundia eum et, his ut nostrum adversarium mediocritatem. Mei ipsum fabulas cotidieque id.		
 	</p>
+	<br>
+	<h4>Contact Information</h4>
+	<p>
+		Phone:	(123)456-7890 <br>
+		Email:	contact@contact.com
+	</p>
+	<br>
+	<button type="submit" class="btn btn-primary">Join Event</button>
 
 <?php include TEMPLATE_BOTTOM; ?>
