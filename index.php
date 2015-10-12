ad85fccb2940b810022e77c75fe4396332b99a9b
Nigel Carter <nigel.carter2@gmail.com>
2015-10-11 23:38:03 -0400

    Made improvements to create account / log in

--- index.php
+++ index.php
@@ -7,65 +7,92 @@
 include TEMPLATE_MIDDLE;
 
 $email_taken = false;
+$bad_login = false;
+
+function doLogin($db, $email, $pass) {
+    $login_query = '
+        SELECT User_id, First_name, Password_hash
+        FROM User U
+        WHERE U.Email = :email
+    ';
+    // Standardize email
+    $email = strtolower($email);
+    
+    $login_params = array(':email' => $email);
+    $result = $db->prepare($login_query);
+    $result->execute($login_params);
+    $row = $result->fetch();
+    $is_match = password_verify($pass, $row['Password_hash']);
+    if ($is_match) {
+        $_SESSION['user'] = $row;
+        header('Location: /');
+        include 'logged_in.php';
+    } else {
+        $bad_login = true;
+        include 'logged_out.php';
+    }
+}
+
+function tryCreateAccount($db, $fname, $lname, $email, $pass) {
+    // Check to see if the email is in use
+    $email = strtolower($email);
+    $email_used_params = array(':email' => $email);
+    $email_used_query = '
+        SELECT COUNT(*) FROM User U
+        WHERE U.Email = :email
+    ';
+    $result = $db->prepare($email_used_query);
+    $result->execute($email_used_params);
+    $email_taken = $result->fetchColumn();
+    if ($email_taken)
+        return false;
+
+    $create_account_params = array(
+        ':fname' => $fname,
+        ':lname' => $lname,
+        ':email' => $email,
+        ':pass' => password_hash($pass, PASSWORD_DEFAULT)
+    );
+    $create_account_query = '
+        INSERT INTO User (
+            First_name,
+            Last_name,
+            Email,
+            Password_hash
+        ) VALUES (
+            :fname,
+            :lname,
+            :email,
+            :pass
+        )
+    ';
+    $result = $db
+        ->prepare($create_account_query)
+        ->execute($create_account_params);
+    return true;
+}
+
 // If the user has submitted the page
 if (!$logged_in && $_SERVER['REQUEST_METHOD'] == 'POST') {
 
     // If the user is creating an account
     if (isset($_POST['createaccount'])) {
-        // Check to see if the email is in use
-        $email_used_params = array(':email' => $_POST['email']);
-        $email_used_query = '
-            SELECT COUNT(*) FROM User U
-            WHERE U.Email = :email
-        ';
-        $result = $db->prepare($email_used_query);
-        $result->execute($email_used_params);
-        $email_taken = $result->fetchColumn();
-        if ($email_taken) {
-            include 'logged_out.php';
+        $success = tryCreateAccount(
+            $db,
+            $_POST['fname'],
+            $_POST['lname'],
+            $_POST['email'],
+            $_POST['pass']
+        );
+        $email_taken = !$success;
+        if ($success) {
+            doLogin($db, $_POST['email'], $_POST['pass']);
         } else {
-            $create_account_params = array(
-                ':fname' => $_POST['fname'],
-                ':lname' => $_POST['lname'],
-                ':email' => $_POST['email'],
-                ':pass' => password_hash($_POST['pass'], PASSWORD_DEFAULT)
-            );
-            $create_account_query = '
-                INSERT INTO User (
-                    First_name,
-                    Last_name,
-                    Email,
-                    Password_hash
-                ) VALUES (
-                    :fname,
-                    :lname,
-                    :email,
-                    :pass
-                )
-            ';
-            $result = $db
-                ->prepare($create_account_query)
-                ->execute($create_account_params);
+            include 'logged_out.php';
         }
     } else if (isset($_POST['login'])) {
-        $login_query = "
-            SELECT User_id, First_name, Password_hash
-            FROM User U
-            WHERE U.Email = :email
-        ";
-        $login_params = array(':email' => $_POST['email']);
-        $pass = $_POST['pass'];
-        $result = $db->prepare($login_query);
-        $result->execute($login_params);
-        $row = $result->fetch();
-        $is_match = password_verify($pass, $row['Password_hash']);
-        if ($is_match) {
-            $_SESSION['user'] = $row;
-            header('Location: index.php');
-            include 'logged_in.php';
-        } else {
-            echo 'Wrong password D:';
-        }
+        // If the user submitted the Log In form
+        doLogin($db, $_POST['email'], $_POST['pass']);
     } else {
         include 'logged_out.php';
     }
--- logged_out.php
+++ logged_out.php
@@ -8,40 +8,47 @@
 			<h4>Log In</h4>
 			<form role="form" action="" method="post">
 				<div class="form-group">
-					<input type="email" class="form-control" name="email" placeholder="Email">
+					<input type="email" class="form-control" name="email" placeholder="Email" required>
 				</div>
-				<div class="form-group">
-					<input type="password" class="form-control" name="pass" placeholder="Password">
+                <?php if ($bad_login): ?>
+                <div class="form-group has-error">
+					<input type="password" class="form-control" name="pass" placeholder="Password" required>
+                    <span id="invalidLogin" class="help-block">Incorrect password or user does not exist.</span>
 				</div>
+                <?php else: ?>
+                <div class="form-group">
+					<input type="password" class="form-control" name="pass" placeholder="Password" required>
+				</div>
+                <?php endif; ?>
 				<button type="submit" name="login" class="btn btn-primary">Log In</button>
 			</form>
 		</div>
 		<?php
-			$fname = $email_taken ? $_POST['fname'] : '';
-			$lname = $email_taken ? $_POST['lname'] : '';
-			$email = $email_taken ? $_POST['email'] : '';
+			$fname = $email_taken ? htmlentities($_POST['fname']) : '';
+			$lname = $email_taken ? htmlentities($_POST['lname']) : '';
+			$email = $email_taken ? htmlentities($_POST['email']) : '';
 		?>
 		<div class="col-xs-6">
 			<h4>Create Account</h4>
 			<form role="form" action="" method="post">
 				<div class="form-group">
-					<input class="form-control" name="fname" placeholder="First name" value="<?=$fname?>">
+					<input class="form-control" name="fname" placeholder="First name" value="<?=$fname?>" required>
 				</div>
 				<div class="form-group">
-					<input class="form-control" name="lname" placeholder="Last name" value="<?=$lname?>">
+					<input class="form-control" name="lname" placeholder="Last name" value="<?=$lname?>" required>
 				</div>
 				<?php if ($email_taken): ?>
 				<div class="form-group has-error">
-					<input type="email" class="form-control" name="email" placeholder="Email" value="<?=$email?>">
+					<input type="email" class="form-control" name="email" placeholder="Email" value="<?=$email?>" required>
 					<span id="invalidEmail" class="help-block">This email has already been used.</span>
 				</div>
 				<?php else: ?>
 				<div class="form-group">
-					<input type="email" class="form-control" name="email" placeholder="Email" value="<?=$email?>">
+					<input type="email" class="form-control" name="email" placeholder="Email" value="<?=$email?>" required>
 				</div>
 				<?php endif; ?>
 				<div class="form-group">
-					<input type="password" class="form-control" name="pass" placeholder="Password">
+					<input type="password" class="form-control" name="pass" placeholder="Password" required>
 				</div>
 				<button type="submit" name="createaccount" class="btn btn-success">Create Account</button>
 			</form>
